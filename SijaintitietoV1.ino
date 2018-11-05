e468bb5b78a98d7fca8a3e1fbb6970b40426e761
terokemp <44669643+terokemp@users.noreply.github.com>
2018-11-05 08:58:58 +0200

    Update SijaintitietoV1.ino

--- SijaintitietoV1.ino
+++ SijaintitietoV1.ino
@@ -12,7 +12,7 @@ void setup()
 void loop()
 {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(989);               // wait for a second
+  delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(989);               // wait for a second
+  delay(1000);               // wait for a second
 }
