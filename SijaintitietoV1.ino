ad429a65d916ae8f6c12077891bd7515067b8f03
Jari Kylmäoja <44605377+jariky@users.noreply.github.com>
2018-11-13 10:26:39 +0200

    Update SijaintitietoV1.ino

--- SijaintitietoV1.ino
+++ SijaintitietoV1.ino
@@ -1,17 +1,155 @@
 
 
-uint8_t led = 9;
+#include "Arduino.h"
+#include <Wire.h>
+
+const uint8_t GPSAddress = 0x42;      // GPS I2C Address
+
+double value;
+double conv;
+
+
+double dataTransfer(int8_t *data_buf, int8_t num)   // Data type converter：convert int8_t type to float
+{                                           //*data_buf:int8_t data array ;num:float length
+  double temp = 0;
+  uint8_t i,j;
+
+  i = data_buf[0] == '-'  ?  1  :  0;
+  
+  while (data_buf[i] != '.')   // The date in the array is converted to an integer and accumulative
+    temp = temp * 10 + (data_buf[i++] - 0x30);
+  for (j=0 ; j<num ; j++)
+    temp = temp * 10 + (data_buf[++i] - 0x30);    
+  for (j=0 ; j<num ; j++)    // The date will converted integer transform into a floating point number
+    temp /= 10;
+
+  if (data_buf[0] == '-') // Negative case
+    temp *= -1;           // Converted to a negative number    
+
+  return temp;
+}
+
+
+void rec_init()   // Initial GPS
+{
+  Wire.beginTransmission(GPSAddress);
+  Wire.write(0xff);   // To send data address      
+  Wire.endTransmission(); 
+
+  Wire.beginTransmission(GPSAddress);
+  Wire.requestFrom(GPSAddress, 10);    // Require 10 bytes read from the GPS device
+}
+
+
+int8_t ID()       // Receive the statement ID
+{
+  int8_t i = 0;
+  int8_t valueI[7] = { '$','G','P','G','G','A',',' };        // To receive the ID content of GPS statements
+  int8_t buff[7] = { '0','0','0','0','0','0','0' };
+
+  while (1)
+  {
+    rec_init();       // Receive data initialization    
+    while ( Wire.available() )
+    { 
+      buff[i] = Wire.read();         // Receive serial data  
+
+      if (buff[i] == valueI[i])      // Contrast the correct ID
+      {
+        if (++i == 7)
+        {
+          Wire.endTransmission();   // End of receiving
+          return 1;                 // Receiving returns 1
+        }
+      }
+      else
+        i=0;
+    }
+
+    Wire.endTransmission();         // End receiving
+  }
+}
+
+
+void rec_data(int8_t *buff, int8_t num1, int8_t num2)   // Receive data function
+{                        //*buff：Receive data array；num1：Number of commas ；num2：The   length of the array
+  int8_t i=0, count=0;
+
+  if ( ID() )
+    while (1)
+    {
+      rec_init();
+
+      while ( Wire.available() )   
+      { 
+        buff[i] = Wire.read();
+
+        if (count != num1)
+        {  
+          if (buff[i] == ',')
+            count++;
+        }
+
+        else if (++i == num2)
+          {
+            Wire.endTransmission();
+            return;
+          }
+      }
+
+      Wire.endTransmission();
+    }
+}
+
+
+//  conv = floor(value/100) * (1 - 1/.6) + value/60;
+
+
+void latitude()     // Latitude information
+{
+  int8_t lat[10] = { '0','0','0','0','0','0','0','0','0','0' };     // Store the latitude data
+  rec_data(lat, 1 ,10);      // Receive the latitude data
+
+  value = dataTransfer(lat, 5);
+  conv = floor(value/100) * 4/-6.0 + value/60;
+  
+  Serial.print("Latitude:\traw = ");
+  Serial.print(value, 8);
+  Serial.print("\tconverted = ");
+  Serial.print(conv, 8);
+}
+
+
+void longitude()     // Longitude information
+{
+  int8_t lon[11] = { '0','0','0','0','0','0','0','0','0','0','0' };   // Store longitude data
+  rec_data(lon, 3, 11);     // Receive the longitude data
+
+  value = dataTransfer(lon, 5);
+  conv = floor(value/100) * 4/-6.0 + value/60;
+  
+  Serial.println();
+  Serial.print("Longitude:\traw = ");
+  Serial.print(value, 8);
+  Serial.print("\tconverted = ");
+  Serial.println(conv, 8);
+}
+
 
 void setup()
 {
-  pinMode(led, OUTPUT);     
+  Wire.begin();          // IIC Initialize
+  Serial.begin(38400);
 }
 
 
 void loop()
 {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(250);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(13);               // wait for a second
+  while (1)
+  {
+    latitude();
+    longitude();
+    Serial.println();
+  }
 }
+
